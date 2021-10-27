39e9ddd72a3c3cd3af299ee856e57108c877f895
Akd <abdelkarimcherif5@gmail.com>
2021-10-27 16:55:59 +0200

    stqrt

---
+++ minion.js
@@ -0,0 +1,13 @@
+/**
+ * Js event handler to change between Clark and Superman modes.
+ *
+ * @author Ezequiel Calvo <ezecafre@gmail.com> 
+ *                        Follow me @EzequielCalvo
+ *                        Hashtag #CSSDrawing #Minion
+ */
+
+ $('.btn').on('click', function() {
+    $('#target').toggleClass('superman');
+    $(this).toggleClass('active');
+  });
+               
